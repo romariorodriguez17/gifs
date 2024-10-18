f6102aaa8d719ff472aa8529751edd53c42a80c2
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-10-18 15:37:01 -0500

    feat(lazy-image) Se añade el nuevo component lazy

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/side-bar/side-bar.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
 
   ],
   imports: [
